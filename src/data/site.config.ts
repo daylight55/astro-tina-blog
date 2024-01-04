6dfe105daaacc67e992a23027e6bed6bbb2fdb92
daylight55 <jojomasanori3@gmail.com>
2024-01-04 22:34:20 +0900

    feat: Add Tina Cloud settings

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,9 +11,9 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'daylight55', // Site author
 	title: '陽のあたらないSomething', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	description: '田舎暮らしエンジニアの独り言、時々サブカル。', // Description to display in the meta tags
+	lang: 'ja',
+	ogLocale: 'ja_JP',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
